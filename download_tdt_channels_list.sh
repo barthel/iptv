9264a69c601274da28221d8123caac9b1dbaad17
barthel <barthel@users.noreply.github.com>
2025-05-23 16:16:26 +0200

    Remove EPG list handling.

--- download_tdt_channels_list.sh
+++ download_tdt_channels_list.sh
@@ -7,25 +7,10 @@ LOCAL_M3U_JSON_FILE="tv.json"
 # Path for storing the hash of the last downloaded M3U JSON file
 M3U_HASH_FILE="tv.hash"
 
-# URL of the M3U JSON file to download
-EPG_XML_URL="https://www.tdtchannels.com/epg/TV.xml.gz"
-LOCAL_EPG_XML_FILE="tv-epg.xml"
-EPG_HASH_FILE="tv-epg.hash"
-
 # Download function for M3U JSON
 download_m3u_json() {
     echo "Downloading M3U JSON file from ${M3U_JSON_URL}..."
     curl --compressed -s -o "${LOCAL_M3U_JSON_FILE}" "${M3U_JSON_URL}"
-}
-
-# Download function for EPG XML
-download_epg_xml() {
-    echo "Downloading EPG XML file from ${EPG_XML_URL}..."
-    curl --compressed -s -o "${LOCAL_EPG_XML_FILE}" "${EPG_XML_URL}"
-    if [[ $(file -b --mime-type "${LOCAL_EPG_XML_FILE}" ) == "application/gzip" ]]; then
-      mv "${LOCAL_EPG_XML_FILE}" "${LOCAL_EPG_XML_FILE}.gz"
-      gzip -d "${LOCAL_EPG_XML_FILE}"
-    fi
 }
 
 # Function to calculate hash and check if the file has changed
@@ -41,23 +26,8 @@ m3u_file_has_changed() {
     return 1  # False that file has not changed
 }
 
-# Function to calculate hash and check if the file has changed
-epg_file_has_changed() {
-    local current_hash
-    current_hash=$(sha256sum "${LOCAL_EPG_XML_FILE}" | awk '{ print $1 }')
-
-    if [ ! -f "${EPG_HASH_FILE}" ] || [ "$(cat "${EPG_HASH_FILE}")" != "${current_hash}" ]; then
-        echo "${current_hash}" > "${EPG_HASH_FILE}"
-        return 0  # True that file has changed
-    fi
-
-    return 1  # False that file has not changed
-}
-
 # Download the M3U JSON file
 download_m3u_json
-# Download the EPG XML file
-download_epg_xml
 
 # Check if the M3U JSON file has changed
 if m3u_file_has_changed; then
@@ -65,10 +35,4 @@ if m3u_file_has_changed; then
   exit 1
 fi
 echo "The M3U JSON file has not changed. No update needed."
-# Check if the EPG XML file has changed
-if epg_file_has_changed; then
-  echo "The EPG XML file has changed. Creating new EPG list..."
-  exit 1
-fi
-echo "The EPG XML file has not changed. No update needed."
 exit 0
