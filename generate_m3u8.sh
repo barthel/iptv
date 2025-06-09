700c31d530a7dace13292790ad59f2dd078e586e
barthel <barthel@users.noreply.github.com>
2025-06-09 11:30:44 +0200

    Add Tlnovelas Europa and MyTime movie network Mexico

--- generate_m3u8.sh
+++ generate_m3u8.sh
@@ -40,4 +40,13 @@ jq -r '
     ) | .[]
 ' "${LOCAL_JSON_FILE}" >> "${M3U_FILE}"
 sed -i "" '/^$/d' "${M3U_FILE}"
+
+# @see: https://github.com/iptv-org/iptv/commit/9302f7fe04f570d53a38b7234d3e95a1d298b947#diff-299e6c124f1e02a3d51557243b1417364c1921684e86824122c117d7fbe56a8dR16-R17
+echo '#EXTINF:-1 tvg-id="null" tvg-name="Tlnovelas Europa" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/6/62/Tlnovelas_logo_2021.png" group-title="Int. Europa",Tlnovelas Europa' >> "${M3U_FILE}"
+echo 'https://televisa-televisa-1-it.samsung.wurl.tv/playlist.m3u8' >> "${M3U_FILE}"
+# @see: https://github.com/iptv-org/iptv/commit/9302f7fe04f570d53a38b7234d3e95a1d298b947#diff-299e6c124f1e02a3d51557243b1417364c1921684e86824122c117d7fbe56a8dR11-R12
+echo '#EXTINF:-1 tvg-id="null" tvg-name="MyTime movie network Mexico" tvg-logo="https://www.linkedin.com/company/mytime-movie-network/" group-title="Int. América",MyTime movie network Mexico' >> "${M3U_FILE}"
+echo 'https://appletree-mytime-samsungmexico.amagi.tv/playlist.m3u8' >> "${M3U_FILE}"
+
+
 echo "M3U playlist has been created: ${M3U_FILE}"
